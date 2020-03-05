ebb241e8b9239a6e6ed78f3d135efb560695866f
Valen Varangu Booth <valen@valenvb.ca>
2020-03-05 12:41:42 -0500

    use toggl-config to store toggle API

--- nodes/start-timer.js
+++ nodes/start-timer.js
@@ -1,18 +1,21 @@
-let TogglClient = require('toggl-api');
 
 module.exports = function(RED) {
     function TogglTimerStartNode(config) {
         RED.nodes.createNode(this,config);
-        let node = this;
-        let toggl = new TogglClient({apiToken:this.credentials.token})
-        node.on('input', function(msg, send) {
-            toggl.startTimeEntry({description: msg.payload}, (err, timeEntry)=>{
+        this.toggl = RED.nodes.getNode(config.toggl).toggl
+                
+        this.on('input', (msg, send)=>{
+            this.toggl.startTimeEntry({description: msg.payload}, (err, timeEntry)=>{
                 if(!err){
                     msg.timeEntry = timeEntry
+                    send(msg)
+                }
+                if(err){
+                    msg.err = err;
                     send(msg)
                 }
             })
         });
     }
-    RED.nodes.registerType("toggl-start-timer",TogglTimerStartNode, {credentials: {token:{type:"text"}}});
+    RED.nodes.registerType("toggl-start-timer",TogglTimerStartNode);
 }
