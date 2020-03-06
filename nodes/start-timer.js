4f9bd907077d9380815672420899e480d5fe328a
Valen Varangu Booth <valen@valenvb.ca>
2020-03-06 18:02:44 -0500

    promisify project lookup

--- nodes/start-timer.js
+++ nodes/start-timer.js
@@ -11,20 +11,72 @@ module.exports = function(RED) {
             pid: config.project,
             tags: config.tags,
         }
-
+        let statusTimeout;
+        
         this.on('input', (msg, send)=>{
-            
-            this.toggl.startTimeEntry(data, (err, timeEntry)=>{
-                if(!err){
-                    msg.payload = timeEntry
-                    send(msg)
-                }
-                if(err){
-                    msg.error = true;
-                    msg.payload = err;
-                    send(msg)
+            if(statusTimeout){
+                clearTimeout(statusTimeout)
+            }
+            this.status({})
+            if(msg.payload){
+                data.description = msg.payload
+            }
+
+            let resolveProject = new Promise( (resolve, reject)=>{
+                if(msg.project){
+                    if(typeof msg.project === "number"){
+                        data.pid = msg.project
+                        resolve()
+                    } else if(typeof msg.project === "string"){
+                        //lookup the project
+                        this.toggl.getWorkspaceProjects(data.wid, (err, projects)=>{
+                            if(err) {
+                                reject(err);
+                                return;
+                            }
+
+                            projects = projects.filter((proj)=>proj.name===msg.project)
+                            if(projects.length>0){
+                                data.pid = projects[0].id
+                                this.status({shape:'ring', fill: 'green', text:"found project"})
+                                resolve()
+                            } else {
+                                reject("No project found")
+                            }
+                        })
+    
+                    }
+                } else {
+                    resolve()
                 }
             })
+
+            resolveProject.then(()=>{
+                this.toggl.startTimeEntry(data, (err, timeEntry)=>{
+                    if(!err){
+                        msg.payload = timeEntry
+                        this.status({shape:'dot', fill: 'green', text:"started"})
+                        send(msg)
+                        
+                        statusTimeout = setTimeout(() => {
+                            this.status({})
+                            clearTimeout(statusTimeout)
+                        }, 2000);
+                    }
+                    if(err){
+                        this.status({shape:'dot', fill: 'red', text:err})
+                        msg.error = true;
+                        msg.payload = err;
+                        send(msg)
+                    }
+                })
+            })
+            .catch(reason=>{
+                this.error(reason)
+                this.status({shape:'dot', fill: 'red', text:reason})
+            })
+            
+            
         });
     }
     RED.nodes.registerType("toggl-start-timer",TogglTimerStartNode);
