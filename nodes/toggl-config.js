dfa246d3fc5a87b4c971c7c44c569bdc17b85138
Valen Varangu Booth <valen@valenvb.ca>
2020-03-05 20:51:59 -0500

    add http endpoints for property window

--- nodes/toggl-config.js
+++ nodes/toggl-config.js
@@ -4,7 +4,40 @@ module.exports = function(RED) {
     function TogglConfigNode(config) {
         RED.nodes.createNode(this,config);
         this.toggl = new TogglClient({apiToken:this.credentials.token})
+
+
+        RED.httpNode.get(`/toggl/${this.id}/workspaces`, (_,res)=>{
+            this.toggl.getWorkspaces((err, data)=>{
+                if(err){
+                    this.error(err)
+                    res.status(500).send(err)
+                }else{
+                    res.send(data)
+                }
+            })
+        })
         
+        RED.httpNode.get(`/toggl/${this.id}/workspace/:ws/projects`, (req,res)=>{
+            this.toggl.getWorkspaceProjects(req.params.ws, (err, data)=>{
+                if(err){
+                    this.error(err)
+                    res.status(500).send(err)
+                }else{
+                    res.send(data)
+                }
+            })
+        })
+        
+        RED.httpNode.get(`/toggl/${this.id}/workspace/:ws/tags`, (req,res)=>{
+            this.toggl.getWorkspaceTags(req.params.ws, (err, data)=>{
+                if(err){
+                    this.error(err)
+                    res.status(500).send(err)
+                }else{
+                    res.send(data)
+                }
+            })
+        })
     }
     RED.nodes.registerType("toggl-config",TogglConfigNode, {credentials: {token:{type:"text"}}});
 }
