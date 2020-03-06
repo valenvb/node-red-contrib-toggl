b9d82dd2a370f6ad652f408dad37a7c254df6f0e
Valen Varangu Booth <valen@valenvb.ca>
2020-03-05 20:54:32 -0500

    build data object from config
    TODO: merge in msg data

--- nodes/start-timer.js
+++ nodes/start-timer.js
@@ -3,9 +3,19 @@ module.exports = function(RED) {
     function TogglTimerStartNode(config) {
         RED.nodes.createNode(this,config);
         this.toggl = RED.nodes.getNode(config.toggl).toggl
-                
+            
+        let data = {
+            created_with: "Node-Red",
+            description : config.description,
+            wid : config.workspace,
+            pid: config.project,
+            tags: config.tags,
+        }
+        
         this.on('input', (msg, send)=>{
-            this.toggl.startTimeEntry({description: msg.payload}, (err, timeEntry)=>{
+            
+
+            this.toggl.startTimeEntry(data, (err, timeEntry)=>{
                 if(!err){
                     msg.timeEntry = timeEntry
                     send(msg)
