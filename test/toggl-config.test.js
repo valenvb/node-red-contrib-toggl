6aee3d417dcb9b8229ffe4be66ef25b1a8019426
Valen Varangu Booth <valen@valenvb.ca>
2020-03-08 18:35:12 -0400

    Mock TogglClient

--- test/toggl-config.test.js
+++ test/toggl-config.test.js
@@ -1,40 +1,50 @@
-let chai = require('chai');
-let expect = chai.expect;
-//const should = chai.should();
-// chai.use(require('chai-things'));
+let chai = require('chai')
+chai.use(require('chai-things'))
+let sinon = require('sinon')
 
-let helper = require("node-red-node-test-helper");
-let configNode = require("../nodes/toggl-config");
-// helper.init(require.resolve('node-red'));
+const rewire = require('rewire')
+
+let helper = require("node-red-node-test-helper")
+let configNode = rewire("../nodes/toggl-config.js")
+
+let MockToggl;
 
 describe("config node", ()=>{
     beforeEach((done)=>{
+        MockToggl = sinon.stub()
+        configNode.__set__('TogglClient', MockToggl)
+
         helper.startServer(done)
     })
 
     afterEach((done)=>{
+        helper.unload()
         helper.stopServer(done)
     })
 
-
-    it.only("can be created", (done)=>{
-        const flow = [ {id:"n1",type:"toggl-config", name: "conf"} ]
-        helper.load([configNode], flow, {config:{token:"bob"}}, ()=>{
-            //let node = helper.getNode('n1')
-            console.log(helper.getNode('n1'))
-            expect(true).to.be.equal(true)
-            // node.should.have.property('name')
+    const flow = [ {id:"conf",type:"toggl-config", name: "Default"} ]
+    
+    it("can be created", (done)=>{
+        helper.load(configNode, flow, {conf:{token:"bob"}}, ()=>{
+            let node = helper.getNode('conf')
+            node.should.have.property('name', "Default")
+            sinon.assert.calledWithNew(MockToggl)
             done()
         })
     })
 
 
-    it("has parameters for an api token", done=>{
+    it("takes an API token parameter", done=>{
         const TOKEN = "bob"
-        helper.load(configNode, flow, {config:{token:TOKEN}}, ()=>{
-            let node = helper.getNode('config')
-            //expect(node.credentials.token).toBe(TOKEN)
+        helper.load(configNode, flow, {conf:{token:TOKEN}}, ()=>{
+            let node = helper.getNode('conf')
+            node.should.have.property('credentials', {token:TOKEN})
+            sinon.assert.calledOnceWithExactly(MockToggl, {apiToken:TOKEN})
             done()
         })
     })
+
+    // describe('node setup APIs', ()=>{
+    
+    // })
 })
