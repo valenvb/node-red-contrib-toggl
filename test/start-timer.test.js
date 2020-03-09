5c9158c7fc628f110dd57414dc5a9aa28ef9562d
Valen Varangu Booth <valen@valenvb.ca>
2020-03-08 22:15:51 -0400

    add sinon-chai

--- test/start-timer.test.js
+++ test/start-timer.test.js
@@ -1,6 +1,9 @@
 let chai = require('chai')
 chai.use(require('chai-things'))
+chai.use(require('sinon-chai'))
+let expect = chai.expect
 let sinon = require('sinon')
+
 
 const rewire = require('rewire')
 
@@ -13,9 +16,13 @@ let startTimerNode = rewire("../nodes/start-timer.js")
 describe("start timer",()=>{
     let MockToggl;
     const Projects = [{id:1,name:"project1"}, {id:2, name:"Project 2"}]
+    let started = sinon.stub()
     beforeEach((done)=>{
         MockToggl = sinon.stub()
-        MockToggl.prototype.startTimeEntry = sinon.stub().callsArgWith(1,null, "fakeEntry")
+
+        MockToggl.prototype.startTimeEntry = function(...args){console.log(args);started()}
+        
+                //sinon.stub().callsArgWith(1,null, "fakeEntry")
         MockToggl.prototype.getWorkspaceProjects = sinon.stub().callsArgWith(1, null, Projects)
         configNode.__set__('TogglClient', MockToggl)
 
@@ -29,7 +36,7 @@ describe("start timer",()=>{
 
     const flow = [ {id:"conf",type:"toggl-config"},{id:"start",type:"toggl-start-timer", toggl:"conf", workspace: "1", wires:[['out']]}, {id:'out', type:'helper'} ]
 
-    it.only("should call startTimeEntry", (done)=>{
+    it("should call startTimeEntry", (done)=>{
         let ExpectData = {
             created_with: "Node-Red",
             description : null,
@@ -43,6 +50,8 @@ describe("start timer",()=>{
             let outNode = helper.getNode('out')
             outNode.on('input', (msg)=>{
                 msg.should.have.property('payload', "fakeEntry")
+                expect(MockToggl.prototype.startTimeEntry).to.have.been.called//With(sinon.match(ExpectData))
+                // sinon.assert.calledWith(MockToggl.prototype.startTimeEntry, ExpectData)
                 done()
             })
             
