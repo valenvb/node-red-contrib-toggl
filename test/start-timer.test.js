fccf1ea1f3d7aab55fbb36063354bc3156585652
Valen Varangu Booth <valen@valenvb.ca>
2020-03-09 17:09:04 -0400

    Bring in real test flow

--- test/start-timer.test.js
+++ test/start-timer.test.js
@@ -17,19 +17,23 @@ describe("start timer",()=>{
         helper.stopServer(done)
     })
 
-    const flow = [ {id:"conf",type:"toggl-config"},{id:"start",type:"toggl-start-timer", toggl:"conf", workspace: "1", wires:[['out']]}, {id:'out', type:'helper'} ]
+    // const flow = [ {id:"conf",type:"toggl-config"},{id:"start",type:"toggl-start-timer", toggl:"conf", workspace: "1", wires:[['out']]}, {id:'out', type:'helper'} ]
+    //const flow = [ {id:"conf",type:"toggl-config"},{id:"start",type:"toggl-start-timer", toggl:"conf", workspace: "1", wires:[[]]} ]
 
-    it("should call startTimeEntry", (done)=>{
-        let ExpectData = {
-            created_with: "Node-Red",
-            description : null,
-            wid : 1,
-            pid : null,
-            tags: null,
-        }
-
-        helper.load([startTimerNode, configNode], flow, {conf:{token:"123"}},()=>{
-            let startNode = helper.getNode('start')
+    const FLOW = [{
+        id: "test",
+        type: "toggl-start-timer",
+        name: "",
+        toggl: "conf",
+        workspace: "1",
+        description: "",
+        tags: [],
+        overwriteTags: false,
+        wires: [['out']]
+    },
+    {id:'out', type:'helper'} ,
+    {id: "conf", type: "toggl-config", name: "Default" }
+    ]
             let outNode = helper.getNode('out')
             outNode.on('input', (msg)=>{
                 msg.should.have.property('payload', "fakeEntry")
